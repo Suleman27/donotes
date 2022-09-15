d1586136c86fc9d00f8532d647eb6e6efa010423
Suleman27 <113366410+Suleman27@users.noreply.github.com>
2022-09-15 22:57:38 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
+const { autoUpdater } = require("electron-updater");
+
 const { app, BrowserWindow } = require('electron');
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
-    /*
-    autoHideMenuBar: true,
-          nodeIntegration: true,
-    webPreferences: {
-      devTools: false 
-      }
-      */
   });
 
-  win.loadFile('index.html')
+  win.loadFile('./src/index.html')
 };
 
 app.whenReady().then(() => {
+  autoUpdater.checkForUpdatesAndNotify()
+
+
   createWindow()
 });
 app.on('window-all-closed', () => {

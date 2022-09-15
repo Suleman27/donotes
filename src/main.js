c24595ae2419938f3a9c35f2d007370563c01e8b
Suleman27 <sulemanhussain271204@gmail.com>
2022-09-15 22:13:58 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    /*
+    autoHideMenuBar: true,
+          nodeIntegration: true,
+    webPreferences: {
+      devTools: false 
+      }
+      */
+  });
+
+  win.loadFile('index.html')
+};
+
+app.whenReady().then(() => {
+  createWindow()
+});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});

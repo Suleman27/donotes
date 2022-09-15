966e067eedeecfc20ec49f3013ab1b4e6f13bf44
Suleman27 <113366410+Suleman27@users.noreply.github.com>
2022-09-15 22:53:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
-    /*
+    
     autoHideMenuBar: true,
           nodeIntegration: true,
     webPreferences: {
       devTools: false 
       }
-      */
     
   });
 

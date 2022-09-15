7a83436dbb62fe0addd2ccad263e7316872fa989
Suleman27 <113366410+Suleman27@users.noreply.github.com>
2022-09-15 23:01:05 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
+    
+    autoHideMenuBar: true,
+          nodeIntegration: true,
+    webPreferences: {
+      devTools: false 
+      }
   });
 
   win.loadFile('./src/index.html')
